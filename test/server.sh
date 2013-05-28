ed57c3458356837cf729913b815bf35841396ed4
Mathieu Turcotte <turcotte.mat@gmail.com>
2013-05-27 20:10:57 -0400

    Add script to launch test server.

---
+++ test/server.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -x
+
+go install github.com/MathieuTurcotte/go-browserchannel/... || exit
+
+GOMAXPROCS=8
+
+bin/server \
+    --public_directory=src/github.com/MathieuTurcotte/go-browserchannel/test/client \
+    --closure_directory=src/github.com/MathieuTurcotte/go-browserchannel/closure \
+    --port=8080 $@
