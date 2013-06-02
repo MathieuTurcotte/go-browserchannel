fb19a8a594e853e2515f414009ff2717115ae7a3
Mathieu Turcotte <turcotte.mat@gmail.com>
2013-06-02 18:13:18 -0400

    Attach an error handler to phantom.onError.

--- test/runner.js
+++ test/runner.js
@@ -116,3 +116,16 @@ function complete() {
 
     phantom.exit(numFail > 0 ? 1 : 0);
 }
+
+phantom.onError = function(msg, trace) {
+    var msgStack = ['PHANTOM ERROR: ' + msg];
+    if (trace && trace.length) {
+        msgStack.push('TRACE:');
+        trace.forEach(function(t) {
+            msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line +
+                (t.function ? ' (in function ' + t.function + ')' : ''));
+        });
+    }
+    console.error(msgStack.join('\n'));
+    phantom.exit(1);
+};
